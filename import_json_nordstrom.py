3ca42458ed5e8cb1b2f153ac3e694170d7c49c51
Will Hui <whui@sajari.com>
2021-10-28 16:04:59 +1100

    Update import_json_nordstrom.py

--- import_json_nordstrom.py
+++ import_json_nordstrom.py
@@ -2,36 +2,47 @@ import json
 import random
 import uuid
 from datetime import tzinfo, timedelta, datetime, timezone
+import sys
 
-with open("./data/nordstrom_data-original.json") as jsonFile:
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+with open(f"./{input_file}") as jsonFile:
     jsonObject = json.load(jsonFile)
     jsonFile.close()
 
 newList = []
-# newCategories = {}
+uniqueSkus = []
 
 
 def genRandSold():
     return random.randint(0, 1000)
 
+
 def genRandMargin():
-    return random.randint(5,50)
+    return random.randint(5, 50)
+
 
 def genRandSale():
     return str(bool(random.getrandbits(1))).lower()
+
 
 def genWarehouseLocation():
     locations = ["Sydney", "Melbourne", "Brisbane"]
     return random.choice(locations)
 
+
 def genUuid():
     return str(uuid.uuid4())
 
+
 def genRatings():
-    return random.randint(1,5)
+    return random.randint(1, 5)
+
 
 def genRatingCount():
-    return random.randint(1,300)
+    return random.randint(1, 300)
+
 
 def genDateAdded():
     start_date = datetime(2021, 1, 1)
@@ -46,6 +57,10 @@ def genDateAdded():
     return int(timestamp)
 
 
+def genStock():
+    return random.randint(0, 150)
+
+
 categories = jsonObject["catalog"][0]["categories"]
 categories_copy = jsonObject["catalog"][0]["categories"]
 products = jsonObject["catalog"][1]["products"]
@@ -58,87 +73,87 @@ for category in categories:
             category["parent_name"] = category_copy["name"]
 
 for product in products:
-    category_hierarchy = []
-    for category in categories:
-        if product["category_id"] == category["id"]:
-            parent_category_name = category["parent_name"]
-            category_name = category["name"]
-            product["category_L1"] = parent_category_name
-            product["category_L2"] = f"{parent_category_name} > {category_name}"
-            category_hierarchy.append(parent_category_name)
-            category_hierarchy.append(category_name)
-    
+    # check if the sku already exists
+    if product["sku"] in uniqueSkus:
+        continue
+    else:
+        # if not add the sku to the uniqueSkus list
+        uniqueSkus.append(product["sku"])
+        category_hierarchy = []
+        for category in categories:
+            if product["category_id"] == category["id"]:
+                parent_category_name = category["parent_name"]
+                category_name = category["name"]
+                product["category_L1"] = parent_category_name
+                product["category_L2"] = f"{parent_category_name} > {category_name}"
+                category_hierarchy.append(parent_category_name)
+                category_hierarchy.append(category_name)
 
-    if "features" in product:
-        for feature in product["features"]:
-            values = list(feature.values())
-            valueName = values[0].lower().replace(' ', '_')
-            value = values[1].lower()
-            product[valueName] = value
-            product.pop("features")
+        if "features" in product:
+            for feature in product["features"]:
+                values = list(feature.values())
+                valueName = values[0].lower().replace(' ', '_')
+                value = values[1].lower()
+                product[valueName] = value
+                product.pop("features")
+
+        if "images" in product:
+            imagesList = [image["url"] for image in product["images"]]
+            product["images_list"] = imagesList
+            product.pop("images")
+
+        if "combinations" in product:
+            variantIds = []
+            quantityList = []
+            priceList = []
+            colorList = []
+            sizeList = []
+            widthList = []
+            for variant in product["combinations"]:
+                variantIds.append(variant["sku"])
+                quantityList.append(variant["quantity"])
+                priceList.append(variant["price"])
+                if "features" in variant:
+                    for feature in variant["features"]:
+                        if feature["name"] == "color":
+                            colorList.append(feature["value"])
+                        if feature["name"] == "size":
+                            sizeList.append(feature["value"])
+                        if feature["name"] == "width":
+                            widthList.append(feature["value"])
+                if "images" in variant:
+                    variantImages = [v["url"] for v in variant["images"]]
+            product.pop("combinations")
+
+            product["variant_ids"] = variantIds
+            product["variant_quantity"] = quantityList
+            product["variant_price"] = priceList
+            product["variant_color"] = colorList
+            product["variant_size"] = sizeList
+            product["variant_width"] = widthList
+            product["variant_images"] = variantImages
+
+        product.pop("reviews_number")
+        product.pop("description")
+        product.pop("meta_description")
+        product.pop("meta_keywords")
+        product.pop("meta_title")
+        product.pop("short_description")
+        product.pop("quantity")
+        product["category_hierarchy"] = category_hierarchy
+        product["margin"] = genRandMargin()
+        product["qty_sold"] = genRandSold()
+        product["sale"] = genRandSale()
+        product["warehouse_location"] = genWarehouseLocation()
+        # product["uuid"] = genUuid() probably don't need uuid as we are only using unique skus
+        product["rating"] = genRatings()
+        product["rating_count"] = genRatingCount()
+        product["date_added"] = genDateAdded()
+        product["stock"] = genStock()
+
+        newList.append(product)
 
 
-    if "images" in product:
-        imagesList = [image["url"] for image in product["images"]]
-        product["images_list"] = imagesList
-        product.pop("images")
-
-    if "combinations" in product:
-        variantIds = []
-        quantityList = []
-        priceList = []
-        colorList = []
-        sizeList = []
-        widthList = []
-        for variant in product["combinations"]:
-            variantIds.append(variant["sku"])
-            quantityList.append(variant["quantity"])
-            priceList.append(variant["price"])
-            if "features" in variant:
-                for feature in variant["features"]:
-                    if feature["name"] == "color":
-                        colorList.append(feature["value"])
-                    if feature["name"] == "size":
-                        sizeList.append(feature["value"])
-                    if feature["name"] == "width":
-                        widthList.append(feature["value"])
-            if "images" in variant:
-                variantImages = [v["url"] for v in variant["images"]]
-        product.pop("combinations")
-                
-
-        product["variant_ids"] = variantIds
-        product["variant_quantity"] = quantityList
-        product["variant_price"] = priceList
-        product["variant_color"] = colorList
-        product["variant_size"] = sizeList
-        product["variant_width"] = widthList
-        product["variant_images"] = variantImages
-
-    product.pop("reviews_number")
-    product.pop("description")
-    product.pop("meta_description")
-    product.pop("meta_keywords")
-    product.pop("meta_title")
-    product.pop("short_description")
-    product["category_hierarchy"] = category_hierarchy
-    product["margin"] = genRandMargin()
-    product["qty_sold"] = genRandSold()
-    product["sale"] = genRandSale()
-    product["warehouse_location"] = genWarehouseLocation()
-    product["uuid"] = genUuid()
-    product["rating"] = genRatings()
-    product["rating_count"] = genRatingCount()
-    product["date_added"] = genDateAdded()
-
-    newList.append(product)
-# print(newList[1])
-
-
-
-
-# sampledList = random.sample(newList,100)
-
-with open('./data/nordstrom_transformed-4.json', 'w') as jsonFile:
+with open(f'./data/{output_file}', 'w') as jsonFile:
     json.dump(newList, jsonFile)
     jsonFile.close()
